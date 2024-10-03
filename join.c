dd19817b52f97747c68da0e6ea1b5161fa595d09
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-10-02 22:14:06 -0700

    Fix and simplify.

--- join.c
+++ join.c
@@ -1,26 +1,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static inline char *split(char *arg) {
+static inline char skip(char *arg) {
     switch (arg[0]) {
-        case ';': case '&': case '|': return arg[1] ? NULL : arg;
-        default: return NULL;
+        case ';': case '&': case '|': return arg[1];
+        default: return 1;
     }
 }
 
 int main(int argc, char *argv[]) {
     int start, index, result;
-    char *join;
+    char *arg;
     for (;;) for (start = index = 1; argv[start]; start = ++index) {
-        while (argv[index] && !(join = split(argv[index]))) ++index;
-        if (!join || !fork()) {
+        while ((arg = argv[index]) && skip(arg)) ++index;
+        if (!arg || !fork()) {
             argv[index] = 0;
             return execvp(argv[start], argv+start);
         }
         result = wait(&result) ? WEXITSTATUS(result) : EXIT_FAILURE;
-        switch (join[0]) {
-            case '&': if (result) return result;
-            case '|': if (!result) return EXIT_SUCCESS;
+        switch (arg[0]) {
+            case '&': if (result) return result; break;
+            case '|': if (!result) return result; break;
         }
     }
 }
